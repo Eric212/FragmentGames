8a31fcb8598efe9201b0f706283c4de81fce9863
Eric <eric@fp.edu.es>
2023-11-07 17:15:04 +0100

    Revert "Añadiendo assets y implementando Toolbar por codigo e include."
    This reverts commit eb66907f2d65bbdcbc0f698e5da52200d787d7b0.

--- app/src/main/java/com/ericsospedra/fragmentsgames/Interfaces/IOnTouchListener.java
+++
@@ -1,8 +0,0 @@
-package com.ericsospedra.fragmentsgames.Interfaces;
-
-import android.view.MotionEvent;
-import android.view.View;
-
-public interface IOnTouchListener {
-    public boolean onTouch(View v, MotionEvent event);
-}
