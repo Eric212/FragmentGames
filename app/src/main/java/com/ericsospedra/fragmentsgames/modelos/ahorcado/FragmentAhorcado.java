696c2bbbc48d438462488e50dcf22166ad40a1d8
Natàlia Sepúlveda Ballester <136610877+natsb7@users.noreply.github.com>
2023-12-04 18:53:18 +0100

    Update FragmentAhorcado.java

--- app/src/main/java/com/ericsospedra/fragmentsgames/modelos/ahorcado/FragmentAhorcado.java
+++ app/src/main/java/com/ericsospedra/fragmentsgames/modelos/ahorcado/FragmentAhorcado.java
@@ -1,5 +1,7 @@
 package com.ericsospedra.fragmentsgames.modelos.ahorcado;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,24 +19,24 @@ import androidx.fragment.app.Fragment;
 import com.ericsospedra.fragmentsgames.R;
 
 public class FragmentAhorcado extends Fragment implements IOnFinish{
-
-    Ahorcado ahorcadoGame;
+    private Ahorcado ahorcadoGame;
     private ImageView ivAhorcado;
     private TextView tvIntentos;
     private TextView tvPalabra;
     private EditText edLetra;
     private TextView tvLetrasIntroducidas;
     private Button bJugar;
+    private Context contexto;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.ahorcado_layout, container, false);
-        //esto sirve para cargar el fragment del ahorcado en el container del main activity
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        contexto = view.getContext();
         ivAhorcado = view.findViewById(R.id.ivAhorcado);
         tvIntentos = view.findViewById(R.id.tvIntentos);
         tvPalabra = view.findViewById(R.id.tvPalabraSecreta);
@@ -58,37 +60,47 @@ public class FragmentAhorcado extends Fragment implements IOnFinish{
     }
 
     private void onButtonClicked(View view) {
-                if(!ahorcadoGame.getGameOver()){
-                    if(ahorcadoGame.getIntentos() > 0){
-                        String letra = edLetra.getText().toString();
-                        String palabraGuiones = ahorcadoGame.comprobarLetra(letra);
-                        tvPalabra.setText(ahorcadoGame.toEspacios(palabraGuiones));
-                        boolean hasGanado = ahorcadoGame.getHasGanado();
-                        if(hasGanado){
-                            Toast.makeText(view.getContext(),  "¡Has ganado! Pulsar el boton 'Jugar' para empezar", Toast.LENGTH_SHORT).show();
-                        }
-                        edLetra.setText("");
-                        if(!ahorcadoGame.getLetraEncontrada()){
-                            ahorcadoGame.registrarFallo(letra);
-                            tvIntentos.setText(String.valueOf(ahorcadoGame.getIntentos()));
-                            tvLetrasIntroducidas.setText(ahorcadoGame.getCadenaLetras());
-                            String fotoName = "hangman_" + ahorcadoGame.getIntentos();
-                            /*int resID = getResources().getIdentifier(fotoName, "drawable", getPackageName());
-                            ivAhorcado.setImageResource(resID);*/
-                        }
-                    }if(ahorcadoGame.getIntentos() == 0){
-                        //Has perdido!
-                        Toast.makeText(view.getContext(), "¡Has perdido! Pulsar el boton 'Jugar' para empezar", Toast.LENGTH_SHORT).show();
-                        tvPalabra.setText(ahorcadoGame.getPalabraSecreta());
-                        ivAhorcado.setImageResource(R.drawable.hangman_0);
+        try{
+            if(!ahorcadoGame.getGameOver()){
+                if(ahorcadoGame.getIntentos() > 0){
+                    String letra = edLetra.getText().toString();
+                    String palabraGuiones = null;
+                    boolean letraValida = ahorcadoGame.esLetraValida(letra);
+                    if (letra.isEmpty() || !letraValida) {
+                        Toast.makeText(view.getContext(), "Por favor, ingresa una letra válida", Toast.LENGTH_SHORT).show();
+                    } else {
+                        palabraGuiones = ahorcadoGame.comprobarLetra(letra);
                     }
-                }else{
-                    ahorcadoGame.createGame();
-                    tvIntentos.setText(String.valueOf(ahorcadoGame.getIntentos()));
-                    tvLetrasIntroducidas.setText("");
-                    tvPalabra.setText(ahorcadoGame.getPalabraGuiones());
-                    tvPalabrita.setText(ahorcadoGame.getPalabraSecreta());
-                    ivAhorcado.setImageResource(R.drawable.hangman_6);
+                    tvPalabra.setText(ahorcadoGame.toEspacios(palabraGuiones));
+                    boolean hasGanado = ahorcadoGame.getHasGanado();
+                    if(hasGanado){
+                        Toast.makeText(view.getContext(),  "¡Has ganado! Pulsar el boton 'Jugar' para empezar", Toast.LENGTH_SHORT).show();
+                    }
+                    edLetra.setText("");
+                    if(!ahorcadoGame.getLetraEncontrada()){
+                        ahorcadoGame.registrarFallo(letra);
+                        tvIntentos.setText(String.valueOf(ahorcadoGame.getIntentos()));
+                        tvLetrasIntroducidas.setText(ahorcadoGame.getCadenaLetras());
+                        Resources res = contexto.getResources();
+                        String fotoName = "hangman_" + ahorcadoGame.getIntentos();
+                        int resID = res.getIdentifier(fotoName, "drawable", contexto.getPackageName());
+                        ivAhorcado.setImageResource(resID);
+                    }
+                }if(ahorcadoGame.getIntentos() == 0){
+                    //Has perdido!
+                    Toast.makeText(view.getContext(), "¡Has perdido! Pulsar el boton 'Jugar' para empezar", Toast.LENGTH_SHORT).show();
+                    tvPalabra.setText(ahorcadoGame.getPalabraSecreta());
+                    ivAhorcado.setImageResource(R.drawable.hangman_0);
+                }
+            }else{
+                ahorcadoGame.createGame();
+                tvIntentos.setText(String.valueOf(ahorcadoGame.getIntentos()));
+                tvLetrasIntroducidas.setText("");
+                tvPalabra.setText(ahorcadoGame.getPalabraGuiones());
+                ivAhorcado.setImageResource(R.drawable.hangman_6);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
